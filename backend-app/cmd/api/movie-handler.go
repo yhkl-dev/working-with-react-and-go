a8d90a3e80aab76d30d45951a88bf7e387958ec2
yhkl-dev <kaiyang939325@gmail.com>
2021-11-14 23:30:44 +0800

    update

--- backend-app/cmd/api/movie-handler.go
+++ backend-app/cmd/api/movie-handler.go
@@ -16,26 +16,38 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 		app.errorJSON(w, err)
 		return
 	}
-	app.logger.Println("id is", id)
-
 	movie, err := app.models.DB.Get(id)
-	// movie := models.Movie{
-	// 	ID:          id,
-	// 	Title:       "some movie",
-	// 	Description: "some description",
-	// 	Year:        2021,
-	// 	ReleaseDate: time.Date(2021, 01, 01, 01, 0, 0, 0, time.Local),
-	// 	RunTime:     100,
-	// 	Rating:      5,
-	// 	MPAARating:  "PG-13",
-	// 	CreatedAt:   time.Now(),
-	// 	UpdatedAt:   time.Now(),
-	// }
-
 	err = app.writeJSON(w, http.StatusOK, movie, "movie")
+	if err != nil {
+		app.errorJSON(w, err)
+	}
 
 }
 
 func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
+	movies, err := app.models.DB.All()
+	if err != nil {
+		app.errorJSON(w, err)
+	}
+	err = app.writeJSON(w, http.StatusOK, movies, "movies")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+}
+
+func (app *application) DeleteMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) InsertMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) UpdateMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) SearchMovie(w http.ResponseWriter, r *http.Request) {
 
 }
--- backend-app/models/models.go
+++ backend-app/models/models.go
@@ -18,17 +18,17 @@ func NewDBModel(db *sql.DB) Models {
 }
 
 type Movie struct {
-	ID          int          `json:"id"`
-	Title       string       `json:"title"`
-	Description string       `json:"description"`
-	Year        int          `json:"year"`
-	ReleaseDate time.Time    `json:"release_date"`
-	RunTime     int          `json:"runtime"`
-	Rating      int          `json:"rating"`
-	MPAARating  string       `json:"mpaa_rating"`
-	CreatedAt   time.Time    `json:"created_at"`
-	UpdatedAt   time.Time    `json:"updated_at"`
-	MovieGenre  []MovieGenre `json:"-"`
+	ID          int            `json:"id"`
+	Title       string         `json:"title"`
+	Description string         `json:"description"`
+	Year        int            `json:"year"`
+	ReleaseDate time.Time      `json:"release_date"`
+	RunTime     int            `json:"runtime"`
+	Rating      int            `json:"rating"`
+	MPAARating  string         `json:"mpaa_rating"`
+	CreatedAt   time.Time      `json:"-"`
+	UpdatedAt   time.Time      `json:"-"`
+	MovieGenre  map[int]string `json:"genres"`
 }
 
 type Genre struct {
@@ -39,10 +39,10 @@ type Genre struct {
 }
 
 type MovieGenre struct {
-	Id        int       `json:"id"`
-	MovieID   int       `json:"movie_id"`
-	GenreID   int       `json:"genre_id"`
+	ID        int       `json:"-"`
+	MovieID   int       `json:"-"`
+	GenreID   int       `json:"-"`
 	Genre     Genre     `json:"genre"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
 }
