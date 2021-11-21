55a60086b33182ed7178e3c06b02fdd9962dfece
yhkl-dev <kaiyang939325@gmail.com>
2021-11-21 19:48:17 +0800

    update

--- backend-app/cmd/api/main.go
+++ backend-app/cmd/api/main.go
@@ -45,10 +45,10 @@ func main() {
 	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
 	flag.StringVar(&cfg.env, "env", "development", "Application environment (development or production)")
 	flag.StringVar(&cfg.db.dsn, "dsn", "postgres://postgres:123456@82.156.172.46:5432/postgres?sslmode=disable", "Postgres connection string ")
-	// flag.StringVar(&cfg.jwt.secret, "jwt-secret", "2dce505d96a53c5768052ee90f3df2055657518dad489160df9913f66042e160", "secret")
+	flag.StringVar(&cfg.jwt.secret, "jwt-secret", "2dce505d96a53c5768052ee90f3df2055657518dad489160df9913f66042e160", "secret")
 	flag.Parse()
 
-	cfg.jwt.secret = os.Getenv("GO_MOVIES_JWT")
+	// cfg.jwt.secret = os.Getenv("GO_MOVIES_JWT")
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 	db, err := openDB(cfg)
@@ -78,7 +78,6 @@ func main() {
 	if err != nil {
 		logger.Println(err)
 	}
-
 }
 
 func openDB(cfg config) (*sql.DB, error) {
